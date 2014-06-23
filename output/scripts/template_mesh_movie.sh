1dee09698fb189b9c4158893c9c1177ae372f9dc
Antonio Tejero <f52tecaa@gmail.com>
2014-06-23 21:15:39 +0200

    BUG FIX: modified bash script for generate mesh movies with a workaround the gnuplot loop bug. This change should be deprecated when new version of gnuplot (with bug fixed) is released

--- output/scripts/template_mesh_movie.sh
+++ output/scripts/template_mesh_movie.sh
@@ -3,9 +3,9 @@
 ### USER CONFIGURATION (USER ACCESIBLE) ###
 
 MESH_TYPE=field
-INI=9
+INI=999
 FIN=200000
-INC=100
+INC=1000
 
 ### SCRIPT CONFIGURATION (NOT USER ACCESIBLE) ###
 
@@ -13,34 +13,35 @@ FILENAME="$MESH_TYPE"_movie
 
 ### GENERATION OF GNUPLOT SCRIPT ###
 
-echo set terminal jpeg size 1280,720 >> plot.gpi
+echo set terminal jpeg size 1280,720 >> plot_"$MESH_TYPE".gpi
 
-echo set nokey >> plot.gpi
-echo set grid >> plot.gpi
-echo set ylabel \""$MESH_TYPE" \(simulation units\)\" >> plot.gpi
-echo set xlabel \"node \(ds = 10% debye lenght\)\" >> plot.gpi
-#echo set xrange [0:12.7] >> plot.gpi
-#echo set yrange [0:102.1] >> plot.gpi
+echo set nokey >> plot_"$MESH_TYPE".gpi
+echo set grid >> plot_"$MESH_TYPE".gpi
+echo set ylabel \""$MESH_TYPE" \(simulation units\)\" >> plot_"$MESH_TYPE".gpi
+echo set xlabel \"node \(ds = 10% debye lenght\)\" >> plot_"$MESH_TYPE".gpi
+#echo set xrange [0:12.7] >> plot_"$MESH_TYPE".gpi
+#echo set yrange [0:102.1] >> plot_"$MESH_TYPE".gpi
 
-echo j=0 >> plot.gpi
+echo j=0 >> plot_"$MESH_TYPE".gpi
 
-echo do for[i=$INI:$FIN:$INC] \{ >> plot.gpi
+echo do for[i=$INI:$FIN/$INC] \{ >> plot_"$MESH_TYPE".gpi
 
-echo set output \""$FILENAME"_\".j.\".jpg\" >> plot.gpi
-echo -e set title \""$MESH_TYPE" \(t = \".i.\"\)\" >> plot.gpi
-echo -e plot \'./"$MESH_TYPE"_t_\'.i.\'.dat\' >> plot.gpi
+echo imod = i*$INC-1 >> plot_"$MESH_TYPE".gpi
+echo set output \""$FILENAME"_\".j.\".jpg\" >> plot_"$MESH_TYPE".gpi
+echo -e set title \""$MESH_TYPE" \(t = \".imod.\"\)\" >> plot_"$MESH_TYPE".gpi
+echo -e plot \'./"$MESH_TYPE"_t_\'.imod.\'.dat\' >> plot_"$MESH_TYPE".gpi
 
-echo j=j+1 >> plot.gpi
+echo j=j+1 >> plot_"$MESH_TYPE".gpi
 
-echo \} >> plot.gpi
+echo \} >> plot_"$MESH_TYPE".gpi
 
 ### EXECUTE GNUPLOT SCRIPT FOR FRAMES GENERATION ###
 
-gnuplot plot.gpi
+gnuplot plot_"$MESH_TYPE".gpi
 
 ### REMOVE GNUPLOT SCRIPT ###
 
-rm plot.gpi
+rm plot_"$MESH_TYPE".gpi
 
 ### GENERATE MOVIE FROM FRAMES AND REMOVE FRAMES ###
 
