9d2dc3ad94e2a2ace193a40fb0b521fbae906326
Antonio Tejero <f52tecaa@gmail.com>
2014-09-22 11:47:06 +0200

    Added script that analize results of simulation and compares with fluid model results

---
+++ output/scripts/template_avg_graph_E2_vs_Phi.rb
@@ -0,0 +1,141 @@
+###------------------ GEMS ------------------###
+require 'gnuplot'
+
+###---------- SCRIPT CONFIGURATION ----------###
+
+# data file parameters
+FIELD_DIR = "../field"
+POTENTIAL_DIR = "../potential"
+AVERAGED = true
+BOT = 2999999
+TOP = 9198000
+STEP = 1000
+NODES = 1000
+GAMMA = 1000
+H = 0.1
+V0 = -0.014142136 #-0.003989423
+PI = Math::PI
+
+# plot parameters 
+param = {:title => "Squared field vs potential (data averaged over #{TOP-BOT} iterations, from iteration #{BOT} to #{TOP})", 
+         :xlabel => "Potential (measured in simulation units)",
+         :ylabel => "Squared electric field (measured in simulation units)"}
+
+###-------------- SCRIPT START --------------###
+
+# format of the file names (KEY will be changed by the value of iter or counter respectively)
+if AVERAGED
+  IFNAME_E = "#{FIELD_DIR}/avg_field_t_KEY.dat"
+  IFNAME_PHI = "#{POTENTIAL_DIR}/avg_potential_t_KEY.dat"
+  OFNAME = "squared_field_vs_potential_from_#{BOT}_to_#{TOP}.jpg"
+else
+  IFNAME_E = "#{FIELD_DIR}/field_t_KEY.dat"
+  IFNAME_PHI = "#{POTENTIAL_DIR}/potential_t_KEY.dat"
+  OFNAME = "squared_field_vs_potential_from_#{BOT}_to_#{TOP}.jpg"
+end
+
+#------------------------------------------------------
+
+#---- Read field and potential data from simulation data files
+field_data = Array.new(NODES+1, 0.0)
+field2_data = Array.new(NODES+1, 0.0)
+potential_data = Array.new(NODES+1, 0.0)
+
+(BOT..TOP).step(STEP) do |iter|
+  f1 = File.open("#{IFNAME_E.gsub("KEY", iter.to_s)}", mode="r")
+  f2 = File.open("#{IFNAME_PHI.gsub("KEY", iter.to_s)}", mode="r")
+  a = f1.readlines
+  b = f2.readlines
+  (0..NODES).step do |index|
+    field_data[index] += a[index].split[1].to_f
+    potential_data[index] += b[index].split[1].to_f
+  end
+  f1.close
+  f2.close
+end
+
+#---- Average data and store file with graphs
+f1 = File.open("averaged_mesh_data.dat", mode="w")
+(0..NODES).step do |index|
+  field_data[index] = field_data[index]/((TOP-BOT)/STEP+1).to_f
+  field2_data[index] = field_data[index]**2
+  potential_data[index] = potential_data[index]/((TOP-BOT)/STEP+1).to_f
+  f1.write("#{index} #{potential_data[index]} #{field_data[index]} #{field2_data[index]}\n")
+end
+f1.close
+
+#---- Obtain probe data
+PHI_P = potential_data[0]
+E_P = field_data[0]
+E_P2 = field2_data[0]
+
+#---- Plot results
+Gnuplot.open do |gp|
+  Gnuplot::Plot.new(gp) do |plot|
+    plot.terminal "jpeg size 1280,720" 
+    #plot.nokey
+    plot.grid
+    plot.ylabel param[:ylabel]
+    plot.xlabel param[:xlabel]
+    plot.title param[:title]
+    plot.output OFNAME.gsub("KEY", $counter.to_s)
+    plot.data = [
+      Gnuplot::DataSet.new( [potential_data, field2_data] ) { |ds|
+        ds.with = "linespoints"
+        ds.title = "PIC Simulations"
+      }, 
+      Gnuplot::DataSet.new( "#{E_P2}+exp(x)*(erf(sqrt(x-#{PHI_P}))+1.)-exp(#{PHI_P})*(1.+2.*sqrt(x-#{PHI_P})/sqrt(#{PI}))+2.*#{V0}*#{V0}*#{GAMMA}*(sqrt(1.-2.*x/(#{GAMMA}*#{V0}*#{V0}))-sqrt(1.-2.*#{PHI_P}/(#{GAMMA}*#{V0}*#{V0})))" ) { |ds|
+        ds.with = "lines"
+        ds.title = "Fluid model prediction"
+      }
+    ]
+  end
+end
+
+####################################################################################################
+#
+#
+#---- Solve fluid model with RK4
+#field_model = Array.new(NODES+1, 0.0)
+#field2_model = Array.new(NODES+1, 0.0)
+#potential_model = Array.new(NODES+1, 0.0)
+#position_model = Array.new(NODES+1, 0.0)
+
+#potential_model[0] = PHI_P
+#field_model[0] = E_P
+#field2_model[0] = E_P*E_P
+#position_model[0] = 0
+#puts "#{position_model[0].to_i} \t #{potential_model[0]} \t #{field_model[0]}"
+
+#def fPhi(e) 
+  #return -e
+#end
+
+#def fE(phi)
+  #return 1.0/(Math.sqrt(1.0-2.0*phi/(GAMMA*V0*V0)))-0.5*Math.exp(phi)*(1.0+Math.erf(Math.sqrt(phi-PHI_P)))
+#end
+
+#(1..NODES-1).step do |index|
+  #position_model[index] = index*H
+
+  #k1Phi = fPhi(field_model[index-1])
+  #k1E = fE(potential_model[index-1])
+
+  #k2Phi = fPhi(field_model[index-1]+H*0.5*k1E)
+  #k2E = fE(potential_model[index-1]+H*0.5*k1Phi)
+
+  #k3Phi = fPhi( field_model[index-1]+H*0.5*k2E)
+  #k3E = fE(potential_model[index-1]+H*0.5*k2Phi)
+
+  #k4Phi = fPhi(field_model[index-1]+H*k3E)
+  #k4E = fE(potential_model[index-1]+H*k3Phi)
+
+  #potential_model[index] = potential_model[index-1]+H*(k1Phi+2.0*k2Phi+2.0*k3Phi+k4Phi)/6.0
+  #field_model[index] = field_model[index-1]+H*(k1E+2.0*k2E+2.0*k3E+k4E)/6.0
+  ##field2_model[index] = field_model[index]**2
+
+  #puts "#{position_model[index].to_i} \t #{potential_model[index]} \t #{field_model[index]}"
+#end
+
+
+
