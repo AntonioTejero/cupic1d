023bf95a1066d2d59b264290fb017f47c627ec79
Antonio Tejero <f52tecaa@gmail.com>
2014-11-04 09:34:31 +0100

    Improved mesh movie generation script

--- output/scripts/template_mesh_movie.rb
+++ output/scripts/template_mesh_movie.rb
@@ -5,45 +5,59 @@ require 'gnuplot'
 
 # data file parameters
 MAGNITUDE = "field"
-AVERAGED = true
-BOT = 999
-TOP = 3436000
-STEP = 1000
+BOT = 100
+TOP = 500000
+STEP = 100
+NODES = 2000
+DS = 1.0e-1
+DT = 1.0e-2
+
 # plot parameters 
-param = {:title => "#{MAGNITUDE} averaged over each #{STEP} iteration (t = KEY)", #KEY will be changed by the value of t
-         :xlabel => "node (ds = 10% debye lenght)",
-         :ylabel => "#{MAGNITUDE} (simulation units)"}
+param = {:title => "#{MAGNITUDE} averaged over each #{STEP} iterations (t = KEY)"+'\n (everything measured in simulation units)', #KEY will be changed by the value of t
+         :xlabel => "position",
+         :ylabel => "#{MAGNITUDE}"}
 
 ###-------------- SCRIPT START --------------###
 
 # format of the file names (KEY will be changed by the value of iter or counter respectively)
-if AVERAGED
-  IFNAME = "avg_#{MAGNITUDE}_t_KEY.dat"
-  OFNAME = "avg_#{MAGNITUDE}_KEY.jpg"
-else
-  IFNAME = "#{MAGNITUDE}_t_KEY.dat"
-  OFNAME = "#{MAGNITUDE}_KEY.jpg"
-end
+IFNAME = "avg_#{MAGNITUDE}_t_KEY.dat"
+OFNAME = "avg_#{MAGNITUDE}_KEY.jpg"
 
 #------------------------------------------------------
 
+#---- Read mesh data
+
 $counter = 0
 Gnuplot.open do |gp|
-    (BOT..TOP).step(STEP) do |iter|
-        Gnuplot::Plot.new(gp) do |plot|
-            plot.terminal "jpeg size 1280,720" 
-            plot.nokey
-            plot.grid
-            plot.ylabel param[:ylabel]
-            plot.xlabel param[:xlabel]
-            plot.title param[:title].gsub("KEY", iter.to_s)
-            plot.output OFNAME.gsub("KEY", $counter.to_s)
-            plot.arbitrary_lines << "plot \"#{IFNAME.gsub("KEY",iter.to_s)}\""
-            $counter += 1
-        end
+  (BOT..TOP).step(STEP) do |iter|
+    position_mesh = Array.new(NODES+1, 0.0)
+    data_mesh = Array.new(NODES+1, 0.0)
+    f1 = File.open(IFNAME.gsub("KEY",iter.to_s), mode="r")
+    a = f1.readlines
+    (0..NODES).step do |index|
+      position_mesh[index] = a[index].split[0].to_f*DS
+      data_mesh[index] = a[index].split[1].to_f
     end
+    f1.close
+    Gnuplot::Plot.new(gp) do |plot|
+      plot.terminal "jpeg size 1280,720" 
+      plot.nokey
+      plot.grid
+      plot.ylabel param[:ylabel]
+      plot.xlabel param[:xlabel]
+      plot.title param[:title].gsub("KEY", (DT*iter).to_s)
+      plot.output OFNAME.gsub("KEY", $counter.to_s)
+      plot.data = [
+        Gnuplot::DataSet.new( [position_mesh, data_mesh] ) { |ds|
+          ds.with = "lines"
+          ds.title = "PIC Simulations"
+        }
+      ]
+      $counter += 1
+    end
+  end
 end
+
 `avconv -f image2 -i #{OFNAME.gsub("KEY", "%d")} -b 32000k #{MAGNITUDE+"_movie.mov"}`
-#`find . -name '*.jpg' -type f -print -delete`
 Dir.glob("*.jpg").each {|f| `rm #{f}`}
 
