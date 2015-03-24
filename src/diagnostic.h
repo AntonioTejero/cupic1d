ac0b2cae83ffae31ab546470571d59052d448235
Antonio Tejero <f52tecaa@gmail.com>
2015-03-24 09:55:14 +0100

    Modified diagnostic module to handle secondary electrons

--- src/diagnostic.h
+++ src/diagnostic.h
@@ -33,7 +33,8 @@ void particles_snapshot(particle *d_p, int num_p, string filename);
 void save_mesh(double *d_m, string filename);
 void save_ddf(double *d_avg_ddf, string filename);
 void save_vdf(double *d_avg_vdf, double vmax, double vmin, string filename);
-void save_log(double t, int num_e, int num_i, double U_e, double U_i, double vd_e, double vd_i, double *d_phi);
+void save_log(double t, int num_e, int num_i, int num_se, double U_e, double U_i, double U_se, double vd_e, double vd_i, 
+              double *d_phi);
 //double calculate_vd_i(double dtin_i);
 
 // device kernels
